781c44bdbb71b8cfb6319741686aa6303bc16a5a
paulmwatson <github@paulmwatson.com>
2010-09-30 22:40:00 +0100

    Basic stacking and loading going on with pinning.

--- js/stackable.js
+++ js/stackable.js
@@ -4,28 +4,63 @@ var stackable = {};
 stackable.openInStack = function(event)
 {
   var ele = $(event.currentTarget);
+  var parentStack = ele.parents('.stackable');
   var href = ele.attr('href');
   
+  if ($('.stackable.pinned').length > 0)
+  {
+    $('.stackable.pinned').animate({'margin-right': '0'}, function()
+    {
+      $(this).removeClass('pinned');
+      stackable.removeBeforeOpenInStack(href, parentStack);
+    });
+  }
+  else
+  {
+    stackable.removeBeforeOpenInStack(href, parentStack);
+  }
+  
+  return false;
+};
+
+stackable.removeBeforeOpenInStack = function(href, parentStack)
+{
+  var eleToRemove = parentStack.nextAll('.stackable');
+  if (eleToRemove.length > 0)
+  {
+    var counter = 0;
+    eleToRemove.fadeOut('fast', function()
+    {
+      counter++;
+      $(this).remove();
+      if (counter === 1)
+      {
+        console.log('d');
+        stackable.openURLInStack(href, parentStack);
+      }
+    });
+  }
+  else
+  {
+    stackable.openURLInStack(href, parentStack);
+  }
+};
+
+stackable.openURLInStack = function(href, parentStack)
+{
   $.get(href, function(data)
   {
-    ele.parents('.stackable').addClass('load_reveal');
-    $('<section class="stackable load_reveal">' + data + '</section>').insertAfter('.stackable:last-child')
+    parentStack.addClass('load_reveal');
+    $('<section class="stackable load_reveal">' + data + '</section>').insertAfter('.stackable:last-child').hide().fadeIn();
   });
-  return false;
 };
 
 stackable.revealStack = function(event)
 {
   var ele = $(event.currentTarget);
   
-  if (ele.is('.load_reveal'))
+  if (!ele.is('.load_reveal') && !ele.is('.pinned'))
   {
-    console.log('load_reveal');
-    ele.removeClass('load_reveal');
-  }
-  else
-  {
-    console.log('not load_reveal');
     ele.next().css('margin-left', '0');
   }
 };
@@ -33,10 +68,36 @@ stackable.revealStack = function(event)
 stackable.unrevealStack = function(event)
 {
   var ele = $(event.currentTarget);
+  ele.removeClass('load_reveal');
   ele.next().css('margin-left', '-10%');
+};
+
+stackable.pinStack = function(event)
+{
+  var ele = $(event.currentTarget);
+  if (ele.is('.pinned'))
+  {
+    ele.animate({'margin-right': '0'}, function()
+    {
+      $(this).removeClass('pinned');
+    });
+  }
+  else
+  {
+    $('.stackable.pinned').animate({'margin-right': '0'}, function()
+    {
+      $(this).removeClass('pinned');
+    });
+    
+    ele.animate({'margin-right': '10%'}, function()
+    {
+      $(this).addClass('pinned');
+    });
+  }
 };
 
 
 $('.opens_in_stack').live('click', stackable.openInStack);
-$('.stackable').live('mouseover', stackable.revealStack);
-$('.stackable').live('mouseout', stackable.unrevealStack);
+//$('.stackable').live('mouseover', stackable.revealStack);
+$//('.stackable').live('mouseleave', stackable.unrevealStack);
+$('.stackable').live('click', stackable.pinStack);
